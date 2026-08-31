d6d3f4e6e2abbea7de9ca01e3b037bae4cae3240
v8wzge <63636409+v8wzge@users.noreply.github.com>
2026-08-31 17:27:59 +0800

    Change Docker image from pttrr to cachyos

--- script-v4.sh
+++ script-v4.sh
@@ -24,7 +24,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v $PWD:/pkg pttrr/docker-makepkg-v4
+    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v $PWD:/pkg cachyos/docker-makepkg-v4
     docker rm kernelbuild
     cd ..
 done
@@ -37,7 +37,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v $PWD:/pkg pttrr/docker-makepkg-v4
+    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v $PWD:/pkg cachyos/docker-makepkg-v4
     docker rm kernelbuild
     cd ..
 done
